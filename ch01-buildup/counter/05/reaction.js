b54a071a3c9841c993b00bceb080f484b1ea77a1
Lee HaYeong <dlgkdud4759@naver.com>
2025-11-26 14:31:09 +0900

    Counter - createRoot(), render() 함수 만들기

---
+++ ch01-buildup/counter/05/reaction.js
@@ -0,0 +1,35 @@
+const reaction = {
+  createElement: (tag, props, ...Children) => {
+    // 요소 노드 생성
+    const elem = document.createElement(tag);
+
+    // 속성 추가
+    if (props) {
+      for (const attrName in props) {
+        const value = props[attrName];
+        elem.setAttribute(attrName, value);
+      }
+    }
+
+    // 자식 노드 추가
+    for (let child of Children) {
+      if (typeof child === "string" || typeof child === "number") {
+        child = document.createTextNode(child);
+      }
+      elem.appendChild(child);
+    }
+
+    // 요소 노드 반환
+    return elem;
+  },
+  // 루트 노드를 관리하는 객체를 반환
+  createRoot: (rootNode) => {
+    return {
+      render: (appFn) => {
+        rootNode.appendChild(appFn());
+      },
+    };
+  },
+};
+
+export default reaction;
