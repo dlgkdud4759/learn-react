bd1573ac362d08dd9966c4d91990fa4a736b9cbb
Lee HaYeong <dlgkdud4759@naver.com>
2025-12-01 16:26:39 +0900

    Todo List - React 환경에서 컴포넌트 분리

---
+++ ch01-buildup/react/06/src/pages/TodoInput.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+interface TodoInputProps {
+  addItem: (title: string) => void;
+}
+
+function TodoInput({ addItem }: TodoInputProps) {
+  // 할일 제목
+  const [title, setTitle] = useState("");
+  console.log("제목 변경됨", title);
+
+  // 추가 버튼 클릭 이벤트 핸들러
+  const handleAdd = () => {
+    console.log("추가 버튼 클릭");
+    // 입력한 제목 꺼내기
+    if (title.trim() !== "") {
+      // 빈 문자열만 입력한게 아니라면
+      addItem(title.trim());
+      setTitle("");
+    }
+  };
+
+  // 엔터 이벤트 핸들러
+  const handleAddKeyUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    console.log("keyup", event);
+    if (event.key === "Enter") handleAdd();
+  };
+
+  return (
+    <div className="todoinput">
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        autoFocus
+        onKeyUp={handleAddKeyUp}
+      />
+      <button type="button" onClick={handleAdd}>
+        추가
+      </button>
+    </div>
+  );
+}
+
+export default TodoInput;
---
+++ ch01-buildup/react/06/src/pages/TodoList.tsx
@@ -0,0 +1,21 @@
+import TodoItem, { type TodoItemType } from "./TodoItem";
+
+interface TodoListProps {
+  itemList: TodoItemType[];
+  toggleDone: (num: number) => void;
+  deleteItem: (num: number) => void;
+}
+
+function TodoList({ itemList, toggleDone, deleteItem }: TodoListProps) {
+  const list = itemList.map((item) => (
+    <TodoItem
+      key={item.num}
+      item={item}
+      toggleDone={toggleDone}
+      deleteItem={deleteItem}
+    />
+  ));
+  return <ul className="todolist">{list}</ul>;
+}
+
+export default TodoList;
