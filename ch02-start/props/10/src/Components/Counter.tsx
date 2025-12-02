ebab35b539ed0f42f6977eada5422c218e1bd026
dlgkdud4759 <dlgkdud4759@naver.com>
2025-12-02 17:25:46 +0900

    ch02 10 Vite로 Counter 감소, 초기화, 증가 기능 추가

--- ch02-start/props/10/src/Components/Counter.tsx
+++ ch02-start/props/10/src/Components/Counter.tsx
@@ -2,12 +2,34 @@ import { useState } from "react";
 
 export function Counter() {
   const [count, setCount] = useState(0);
+
+  // 카운터 감소
+  const countDown = () => {
+    setCount(count - 1);
+  };
+
+  // 카운터 초기화
+  const countReset = () => {
+    setCount(0);
+  };
+
+  // 카운터 증가
+  const countUp = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div id="counter">
-      <button type="button">-_-</button>
-      <button type="button">0_0</button>
-      <button type="button">+_+</button>
-      <span>0</span>
+      <button type="button" onClick={countDown}>
+        -_-
+      </button>
+      <button type="button" onClick={countReset}>
+        0_0
+      </button>
+      <button type="button" onClick={countUp}>
+        +_+
+      </button>
+      <span>{count}</span>
     </div>
   );
 }
