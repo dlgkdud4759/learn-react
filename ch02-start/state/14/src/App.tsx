b592bc5b5a7dc1b04823ab4a150c3158e1e17e09
Lee HaYeong <dlgkdud4759@naver.com>
2025-12-04 14:50:35 +0900

    ch02 - 14 Vite 환경세팅 및 상태관리 대상이 복합 객체일 경우 불변성

---
+++ ch02-start/state/14/src/App.tsx
@@ -0,0 +1,96 @@
+import { Fragment, useState } from "react";
+import UserInfo from "./components/UserInfo";
+import type { User } from "./types";
+
+function App() {
+  const initialUser: User = {
+    _id: 4,
+    email: "u1@market.com",
+    name: "데이지",
+    phone: "01044445555",
+    address: "서울시 강남구 논현동 222",
+    type: "user",
+    createdAt: "2025.05.25 21:08:14",
+    updatedAt: "2025.06.04 09:38:14",
+    extra: {
+      birthday: "11-30",
+      addressBook: [
+        {
+          id: 1,
+          name: "회사",
+          value: "서울시 강동구 천호동 123",
+        },
+        {
+          id: 2,
+          name: "집",
+          value: "서울시 강동구 성내동 234",
+        },
+      ],
+    },
+  };
+
+  const [user, setUser] = useState(initialUser);
+  const [prevUser, setPrevUser] = useState(user);
+
+  // 주소가 수정될 때 호출되는 이벤트 핸들러
+  function handleAddressChange(id: number, value: string) {
+    console.log(id, value);
+    // user 상태를 변경해서 리렌더링
+    // 상태의 불변성이 지켜지지 않음
+    // const newUser = { ...user }; // 주소 변경 후의 데이터
+    // const targetAddress = user.extra.addressBook.find(
+    //   (address) => address.id === id
+    // );
+    // if (targetAddress) {
+    //   targetAddress.value = value;
+    // }
+
+    // 상태의 불변성을 지키기 위해서 추가 작업이 필요
+    const newAddressBook = user.extra.addressBook.map((address) => {
+      if (address.id === id) {
+        return { ...address, value };
+      } else {
+        return address;
+      }
+    });
+
+    const newUser = {
+      ...user,
+      extra: { ...user.extra, addressBook: newAddressBook },
+    };
+
+    setPrevUser(prevUser);
+    setUser(newUser);
+  }
+
+  const list = user.extra.addressBook.map((address) => {
+    return (
+      <Fragment key={address.id}>
+        <label htmlFor="1">{address.name}</label>
+        <input
+          id={address.id.toString()}
+          type="text"
+          name={address.name.toString()}
+          value={address.value}
+          onChange={(event) =>
+            handleAddressChange(address.id, event.target.value)
+          }
+        />
+        <br />
+      </Fragment>
+    );
+  });
+
+  return (
+    <>
+      <h1>14 상태관리 대상이 복합 객체일 경우 불변성 (feat. immer)</h1>
+      <div style={{ display: "flex", gap: "1rem" }}>
+        <UserInfo title="변경 전" user={prevUser} />
+        <UserInfo title="변경 후" user={user} />
+      </div>
+      <p>{list}</p>
+    </>
+  );
+}
+
+export default App;
---
+++ ch02-start/state/14/src/components/UserInfo.tsx
@@ -0,0 +1,31 @@
+import type { User } from "../types";
+
+interface UserInfoProps {
+  title: string;
+  user: User;
+}
+
+function UserInfo({ title, user }: UserInfoProps) {
+  return (
+    <div style={{ border: "1px solid", padding: "1rem", borderRadius: "10px" }}>
+      <h2>{title}</h2>
+      <p>
+        이메일: {user.email}
+        <br />
+        이름: {user.name}
+        <br />
+        전화번호: {user.phone}
+        <br />
+      </p>
+      <ul>
+        {user.extra.addressBook.map((address) => (
+          <li key={address.id}>
+            {address.name}: {address.value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default UserInfo;
