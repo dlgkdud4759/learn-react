3e0aca4952b8916279e7df40a380fee32c8deb51
Lee HaYeong <dlgkdud4759@naver.com>
2025-12-08 13:20:43 +0900

    ch03 - 02 Vite 환경 세팅 및 함수형 컴포넌트로 구현

---
+++ ch03-class/02-functionbase/src/App.tsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import ClickMe from "./ClickMe";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>02 클래스 컴포넌트 - 함수형 컴포넌트와 같이 사용</h1>
+        <ClickMe level={2} />
+        <ClickMe level={3} />
+      </div>
+    );
+  }
+}
+
+export default App;
