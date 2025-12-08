9a67f5f95576167256a931a92c1b04afc10110d2
Lee HaYeong <dlgkdud4759@naver.com>
2025-12-08 11:04:42 +0900

    ch03 - 폴더 세팅 및 01 예제 Vite 환경 세팅

---
+++ ch03-class/01-classbase/src/App.tsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import ClickMe from "./ClickMe";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>01 클래스 컴포넌트</h1>
+        <ClickMe />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ ch03-class/01-classbase/src/ClickMe.tsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class ClickMe extends Component {
+  render() {
+    return (
+      <div>
+        클릭 횟수 X 1: 0<button>클릭</button>
+      </div>
+    );
+  }
+}
+
+export default ClickMe;
