efb784667b90ce1beca0bfdc96ba3b19c5a0782f
Lee HaYeong <dlgkdud4759@naver.com>
2025-12-11 00:05:38 +0900

    [과제] TodoInput 컴포넌트에서 useRef를 사용해 새로운 아이템이 추가된 후 input 요소에 포커스가 갈수 있게 처리

--- ch12-app/todolist/03-hooks/src/pages/TodoInput.tsx
+++ ch12-app/todolist/03-hooks/src/pages/TodoInput.tsx
@@ -1,32 +1,45 @@
-import { useState, type KeyboardEvent } from "react";
+import { useRef, useState, type KeyboardEvent } from "react";
 
 interface TodoInputProps {
   addItem: (title: string) => void;
 }
 
-function TodoInput({ addItem }: TodoInputProps){
-  console.log('\t\tTodoInput 렌더링');
+function TodoInput({ addItem }: TodoInputProps) {
+  console.log("\t\tTodoInput 렌더링");
 
-  const [title, setTitle] = useState('');
+  const [title, setTitle] = useState("");
+  const titleElem = useRef<HTMLInputElement>(null);
 
   // 추가 버튼 클릭 이벤트 핸들러
+  // 과제 1. TodoInput 컴포넌트에서 useRef를 사용해 새로운 아이템이 추가된 후 input 요소에 포커스가 갈수 있게 처리
   const handleAdd = () => {
-    if(title.trim() !== ''){
+    if (title.trim() !== "") {
       addItem(title);
-      setTitle('');
+      setTitle("");
+
+      titleElem.current?.focus();
     }
   };
 
   // 엔터 이벤트 핸들러
   const handleAddKeydown = (event: KeyboardEvent) => {
-    if(event.nativeEvent.isComposing) return; // 글자가 완성되지 않았을 경우 무시한다.(Mac 사용자)
-    if(event.key === 'Enter') handleAdd();
+    if (event.nativeEvent.isComposing) return; // 글자가 완성되지 않았을 경우 무시한다.(Mac 사용자)
+    if (event.key === "Enter") handleAdd();
   };
 
   return (
     <div className="todoinput">
-      <input type="text" autoFocus value={ title } onChange={ (e) => setTitle(e.target.value) } onKeyDown={ handleAddKeydown } />
-      <button type="button" onClick={ handleAdd }>추가</button>
+      <input
+        ref={titleElem}
+        type="text"
+        autoFocus
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        onKeyDown={handleAddKeydown}
+      />
+      <button type="button" onClick={handleAdd}>
+        추가
+      </button>
     </div>
   );
 }
