32c3499b17a1a2098b5eb6e5ecb7ecc52e0acd21
Lee HaYeong <dlgkdud4759@naver.com>
2025-12-11 00:55:43 +0900

    [과제] TodoContainer 컴포넌트에서 useRef를 사용해 nextId를 만들고 할일 추가시 nextId를 사용해서 _id 값을 만들고 1씩 증가

--- ch12-app/todolist/03-hooks/src/pages/TodoContainer.tsx
+++ ch12-app/todolist/03-hooks/src/pages/TodoContainer.tsx
@@ -1,38 +1,53 @@
 import Todo from "@pages/Todo";
 import type { TodoItem } from "@pages/TodoItem";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
-function TodoContainer(){
+// 과제 2. TodoContainer 컴포넌트에서 useRef를 사용해 nextId를 만들고 할일 추가시 nextId를 사용해서 _id 값을 만들고 1씩 증가
+function TodoContainer() {
   // 샘플 목록
   const initItemList: TodoItem[] = [
-    { _id: 1, title: '자바스크립트 공부', done: true },
-    { _id: 2, title: 'JS 프로젝트', done: true },
-    { _id: 3, title: 'React 공부', done: false },
+    { _id: 1, title: "자바스크립트 공부", done: true },
+    { _id: 2, title: "JS 프로젝트", done: true },
+    { _id: 3, title: "React 공부", done: false },
   ];
 
   // 상태가 수정되면 자동으로 화면이 리렌더링 된다.
-  const [ itemList, setItemList ] = useState(initItemList);
+  const [itemList, setItemList] = useState(initItemList);
+
+  const nextId = useRef(itemList.length + 1); // { current: 4 } 객체를 반환
 
   // 할일 추가
   const addItem = (title: string) => {
-    const item: TodoItem = { _id: itemList[itemList.length-1]?._id + 1 || 1, title, done: false };
-    setItemList([ ...itemList, item ]);
-  }
+    const item: TodoItem = {
+      _id: nextId.current,
+      title,
+      done: false,
+    };
+    nextId.current += 1;
+    setItemList([...itemList, item]);
+  };
 
   // 완료/미완료 처리
   const toggleDone = (_id: number) => {
-    const newItemList = itemList.map(item => item._id === _id ? { ...item, done: !item.done } : item);
+    const newItemList = itemList.map((item) =>
+      item._id === _id ? { ...item, done: !item.done } : item
+    );
     setItemList(newItemList);
-  }
+  };
 
   // 할일 삭제
   const deleteItem = (_id: number) => {
-    const newItemList = itemList.filter(item => item._id !== _id );
+    const newItemList = itemList.filter((item) => item._id !== _id);
     setItemList(newItemList);
-  }
+  };
 
   return (
-    <Todo itemList={ itemList } addItem={ addItem } toggleDone={ toggleDone } deleteItem={ deleteItem } />
+    <Todo
+      itemList={itemList}
+      addItem={addItem}
+      toggleDone={toggleDone}
+      deleteItem={deleteItem}
+    />
   );
 }
 
