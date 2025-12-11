8137c7903e95d375c4d360272c4fac30aaceceb8
Lee HaYeong <dlgkdud4759@naver.com>
2025-12-12 01:25:27 +0900

    [과제] React.memo()를 사용해서 불필요한 리렌더링을 방지

--- ch12-app/todolist/04-memo/src/pages/TodoItem.tsx
+++ ch12-app/todolist/04-memo/src/pages/TodoItem.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export interface TodoItem {
   _id: number;
   title: string;
@@ -10,15 +12,21 @@ interface TodoItemProps {
   deleteItem: (_id: number) => void;
 }
 
-function TodoItem({ item, toggleDone, deleteItem }: TodoItemProps){
-  console.log('\t\t\t\tTodoItem 렌더링', item);
+// 과제 1. React.memo()를 사용해서 불필요한 리렌더링을 방지
+function TodoItem({ item, toggleDone, deleteItem }: TodoItemProps) {
+  console.log("\t\t\t\tTodoItem 렌더링", item);
   return (
     <li>
-      <span>{ item._id }</span>
-      <span onClick= { () => toggleDone(item._id) }>{ item.done ? <s>{ item.title }</s> : item.title }</span>
-      <button type="button" onClick={ () => deleteItem(item._id) }>삭제</button>
+      <span>{item._id}</span>
+      <span onClick={() => toggleDone(item._id)}>
+        {item.done ? <s>{item.title}</s> : item.title}
+      </span>
+      <button type="button" onClick={() => deleteItem(item._id)}>
+        삭제
+      </button>
     </li>
   );
 }
 
-export default TodoItem;
+// export default TodoItem;
+export default React.memo(TodoItem);
