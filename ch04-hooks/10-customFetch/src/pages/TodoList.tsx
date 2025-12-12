56bf9224c69bf0dc9631a83b99378142702c0bae
Lee HaYeong <dlgkdud4759@naver.com>
2025-12-12 13:16:39 +0900

    ch04 - 10 컴포넌트 분리 및 customHook - useFatch, useAxios 사용

---
+++ ch04-hooks/10-customFetch/src/pages/TodoList.tsx
@@ -0,0 +1,23 @@
+import useFetch from "@/hooks/useFetch";
+
+function TodoList() {
+  const { data, error, loading } = useFetch({ url: "/todolist" });
+  const list = data?.items.map((item) => <li key={item._id}>{item.title}</li>);
+  return (
+    <>
+      <h1>10 customHook - useFatch, useAxios 커스텀 훅 사용</h1>
+      <h2>할일 목록</h2>
+
+      {/* <!-- 로딩중일 때 로딩중 메시지 표시 --> */}
+      {loading && <p>로딩중...</p>}
+
+      {/* <!-- 에러가 있을 경우 빨간색으로 에러 메시지 표시 --> */}
+      {error && <p style={{ color: "red" }}>네트워크 연결 오류</p>}
+
+      {/* <!-- 서버에서 받은 Todo 목록을 렌더링 --> */}
+      <ul>{data && list}</ul>
+    </>
+  );
+}
+
+export default TodoList;
