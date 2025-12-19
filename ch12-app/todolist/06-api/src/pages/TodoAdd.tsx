d3332f6639a1a78c3bb738b0653ed5ea3d071748
Lee HaYeong <dlgkdud4759@naver.com>
2025-12-19 15:51:04 +0900

    ch12 - 06 할일을 추가중일 때는 버튼 비활성화

--- ch12-app/todolist/06-api/src/pages/TodoAdd.tsx
+++ ch12-app/todolist/06-api/src/pages/TodoAdd.tsx
@@ -1,6 +1,11 @@
-import { Form, Link } from "react-router";
+import { Form, Link, useNavigation } from "react-router";
 
 function TodoAdd() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading"; // loader 실행중
+  const isSubmitting = navigation.state === "submitting"; // action 실행중
+  const isProcessing = isSubmitting || isLoading; // 작업중
+
   return (
     <div id="main">
       <h2>할일 추가</h2>
@@ -12,7 +17,9 @@ function TodoAdd() {
           <label htmlFor="content">내용 :</label>
           <textarea id="content" name="content" cols={23} rows={5}></textarea>
           <br />
-          <button type="submit">추가</button>
+          <button type="submit" disabled={isProcessing}>
+            {isProcessing ? "추가중..." : "추가"}
+          </button>
           <Link to="/todo/list">취소</Link>
         </Form>
       </div>
