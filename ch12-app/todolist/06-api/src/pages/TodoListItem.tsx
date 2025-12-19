3be224a9ea6e94e43cc55b1a6e0d7782cef2db21
Lee HaYeong <dlgkdud4759@naver.com>
2025-12-19 15:34:02 +0900

    ch12 - 06 삭제 기능 구현 완료

--- ch12-app/todolist/06-api/src/pages/TodoListItem.tsx
+++ ch12-app/todolist/06-api/src/pages/TodoListItem.tsx
@@ -1,16 +1,28 @@
 import type { Todo } from "@/types/todo";
-import { Link } from "react-router";
+import { Link, useFetcher } from "react-router";
 
 interface TodoListItemProps {
   item: Todo;
 }
 
 function TodoListItem({ item }: TodoListItemProps) {
+  // 페이지 이동 없이 action이나 loader를 직접 호출
+  const fetcher = useFetcher();
+
+  const handleDelete = () => {
+    // fetcher.submit(formData, options)
+    fetcher.submit(null, {
+      method: "delete",
+      action: `/todo/list/${item._id}`, // 호출할 action을 등록한 path를 지정
+    }); // action을 직접 호출
+  };
   return (
     <li>
       <span>{item._id}</span>
       <Link to={`/todo/list/${item._id}`}>{item.title}</Link>
-      <Link to="/todo/list">삭제</Link>
+      <button type="button" onClick={handleDelete}>
+        삭제
+      </button>
     </li>
   );
 }
