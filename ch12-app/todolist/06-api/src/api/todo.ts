f9fb4e0bc826308e597ddc3a012dd839452d5422
Lee HaYeong <dlgkdud4759@naver.com>
2025-12-19 13:13:19 +0900

    ch12 - 06 할일 수정 및 삭제 API 생성

--- ch12-app/todolist/06-api/src/api/todo.ts
+++ ch12-app/todolist/06-api/src/api/todo.ts
@@ -48,3 +48,42 @@ export async function createTodo(
   }
   return data;
 }
+
+// 할일 수정
+export async function updateTodo(
+  _id: number,
+  formData: FormData
+): Promise<ResData<TodoInfoRes>> {
+  // https://github.com/FEBC-15/js/blob/main/docs/09.js_ajax.md#52-fetchresource-options
+  const body = {
+    title: formData.get("title"),
+    content: formData.get("content"),
+    done: formData.get("done") === "on",
+  };
+  const res = await fetch(`${API_URL}/todolist/${_id}`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" }, // 브라우저가 보내는 바디 데이터의 타입 지정
+    body: JSON.stringify(body),
+  });
+  const data = await res.json();
+
+  if (!res.ok) {
+    // 서버의 응답 상태코드가 4xx, 5xx인 에러일 경우 라우터 에러로 변환
+    throw new Response(data.message, { status: res.status });
+  }
+  return data;
+}
+
+// 할일 삭제
+export async function deleteTodo(_id: number): Promise<ResData<TodoInfoRes>> {
+  const res = await fetch(`${API_URL}/todolist/${_id}`, {
+    method: "DELETE",
+  });
+  const data = await res.json();
+
+  if (!res.ok) {
+    // 서버의 응답 상태코드가 4xx, 5xx인 에러일 경우 라우터 에러로 변환
+    throw new Response(data.message, { status: res.status });
+  }
+  return data;
+}
