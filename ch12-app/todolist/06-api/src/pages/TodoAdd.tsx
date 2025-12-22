ff8fc19f034a7152d6365cbc532eae322800bd88
Lee HaYeong <dlgkdud4759@naver.com>
2025-12-22 13:05:37 +0900

    ch12 - 06 할일 추가 부분(카테고리, 중요, 마감일 정보 추가)

--- ch12-app/todolist/06-api/src/pages/TodoAdd.tsx
+++ ch12-app/todolist/06-api/src/pages/TodoAdd.tsx
@@ -17,6 +17,20 @@ function TodoAdd() {
           <label htmlFor="content">내용 :</label>
           <textarea id="content" name="content" cols={23} rows={5}></textarea>
           <br />
+          <label htmlFor="category">카테고리: </label>
+          <select id="category" name="category">
+            <option value="">선택하세요.</option>
+            <option value="study">공부</option>
+            <option value="hubby">취미</option>
+            <option value="etc">기타</option>
+          </select>
+          <br />
+          <label htmlFor="important">중요: </label>
+          <input type="checkbox" id="important" name="important" />
+          <br />
+          <label htmlFor="finishAt">마감일: </label>
+          <input type="datetime-local" id="finishAt" name="finishAt" />
+          <br />
           <button type="submit" disabled={isProcessing}>
             {isProcessing ? "추가중..." : "추가"}
           </button>
