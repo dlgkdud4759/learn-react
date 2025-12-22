25eba732fd3b5527d2a498e79ee4157b94e6168b
Lee HaYeong <dlgkdud4759@naver.com>
2025-12-22 15:28:38 +0900

    ch06 - Context 객체 생성 및  자식 컴포넌트에 제공

--- ch06-contextapi/01-prop-drilling/src/App.tsx
+++ ch06-contextapi/01-prop-drilling/src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Left1 from "@/components/Left1";
 import Right1 from "@/components/Right1";
 import "./App.css";
+import { CounterProvider } from "./contexts/CounterContext";
 
 function App() {
   // count 정의
@@ -22,8 +23,11 @@ function App() {
       <div id="container">
         <h1>App</h1>
         <div id="grid">
-          <Left1 count={count} /> {/* props로 count 전달 */}
-          <Right1 countUp={countUp} /> {/* props로 countUp 전달 */}
+          {/* 자식 컴포넌트에 Context 제공 */}
+          <CounterProvider>
+            <Left1 count={count} /> {/* props로 count 전달 */}
+            <Right1 countUp={countUp} /> {/* props로 countUp 전달 */}
+          </CounterProvider>
         </div>
       </div>
     </>
