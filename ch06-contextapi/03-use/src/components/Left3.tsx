57044ef224b1bd3164d3e2b7b3e97bd9dacc18b9
Lee HaYeong <dlgkdud4759@naver.com>
2025-12-23 09:49:45 +0900

    ch06 - 03 use를 사용해 조건부 컨텍스트 구독

--- ch06-contextapi/03-use/src/components/Left3.tsx
+++ ch06-contextapi/03-use/src/components/Left3.tsx
@@ -1,14 +1,18 @@
 import CounterContext from "@/contexts/CounterContext";
-import { useContext, useEffect } from "react";
+import { use, useEffect } from "react";
 
-function Left3() {
+function Left3({ showcounter = true }) {
   useEffect(() => {
     console.log("#### Left3 렌더링.");
   });
 
-  // 4. 자식 컴포넌트에서 Context 사용
-  // 4-1. useContext 훅 사용
-  const counter = useContext(CounterContext);
+  let counter = null;
+
+  // context 사용하기
+  // use를 이용하면 조건부로 컨텍스트 구독이 가능하므로 불필요한 리렌더링 방지
+  if (showcounter) {
+    counter = use(CounterContext);
+  }
   return (
     <div>
       <h3>Left3</h3>
--- ch06-contextapi/03-use/src/components/Right3.tsx
+++ ch06-contextapi/03-use/src/components/Right3.tsx
@@ -1,20 +1,19 @@
 import CounterContext from "@/contexts/CounterContext";
-import { useContext, useEffect } from "react";
+import { use, useEffect } from "react";
 
 function Right3() {
   useEffect(() => {
     console.log("#### Right3 렌더링.");
   });
 
-  // 4. 자식 컴포넌트에서 Context 사용
-  // 4-1. useContext 훅 사용
-  const counter = useContext(CounterContext);
+  // context 사용하기
+  const count = use(CounterContext);
   return (
     <div>
       <h3>Right3</h3>
-      <button onClick={() => counter?.countUp(1)}>+1</button>
-      <button onClick={() => counter?.reset(0)}>0</button>
-      <button onClick={() => counter?.countDown(1)}>-1</button>
+      <button onClick={() => count?.countUp(1)}>+1</button>
+      <button onClick={() => count?.reset(0)}>0</button>
+      <button onClick={() => count?.countDown(1)}>-1</button>
     </div>
   );
 }
