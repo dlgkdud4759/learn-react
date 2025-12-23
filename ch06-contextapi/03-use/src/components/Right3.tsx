54a2561ffdb6653c7c7bf77f509879203fd4241a
Lee HaYeong <dlgkdud4759@naver.com>
2025-12-23 09:37:24 +0900

    ch06 - 03 Vite 환경 세팅(use)

--- ch06-contextapi/03-use/src/components/Right3.tsx
+++ ch06-contextapi/03-use/src/components/Right3.tsx
@@ -1,13 +1,20 @@
-import { useEffect } from 'react';
+import CounterContext from "@/contexts/CounterContext";
+import { useContext, useEffect } from "react";
 
 function Right3() {
-  useEffect(()=>{
-    console.log('#### Right3 렌더링.');
+  useEffect(() => {
+    console.log("#### Right3 렌더링.");
   });
+
+  // 4. 자식 컴포넌트에서 Context 사용
+  // 4-1. useContext 훅 사용
+  const counter = useContext(CounterContext);
   return (
     <div>
       <h3>Right3</h3>
-      <button onClick={ () => {} }>+1</button>
+      <button onClick={() => counter?.countUp(1)}>+1</button>
+      <button onClick={() => counter?.reset(0)}>0</button>
+      <button onClick={() => counter?.countDown(1)}>-1</button>
     </div>
   );
 }
