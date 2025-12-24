4a61e6142418cc3df8bbf449f0e7ce733258318b
Lee HaYeong <dlgkdud4759@naver.com>
2025-12-24 16:44:57 +0900

    ch08 - 04 tailwind css 적용

--- ch08-css/04-tailwind/src/App.tsx
+++ ch08-css/04-tailwind/src/App.tsx
@@ -1,14 +1,17 @@
 import Button from "@components/ui/Button";
 
-function App(){
+function App() {
   return (
     <>
-      <h1>04 Tailwind CSS</h1>
+      <h1 className="text-4xl font-bold my-6 block">04 Tailwind CSS</h1>
 
       <Button>그냥 버튼</Button>
-      <Button type="reset" bg="blue" color="red">취소 버튼</Button>
-      <Button type="submit" bg="gray" color="blue">submit 버튼</Button>
-
+      <Button type="reset" bg="red" color="white">
+        취소 버튼
+      </Button>
+      <Button type="submit" bg="blue" color="white">
+        submit 버튼
+      </Button>
     </>
   );
 }
--- ch08-css/04-tailwind/src/components/ui/Button.tsx
+++ ch08-css/04-tailwind/src/components/ui/Button.tsx
@@ -1,13 +1,46 @@
-import styles from './Button.module.css';
+const btnBg = {
+  gray: "bg-gray-500",
+  blue: "bg-blue-500",
+  red: "bg-red-500",
+  white: "bg-white-500",
+} as const; // 모든 속성이 readonly가 되고 각 속성의 타입이 string이 아니라 리터럴로 추론됨
+// const btnBg: {
+//    readonly gray: "bg-gray-500";
+//    readonly blue: "bg-blue-500";
+//    readonly red: "bg-red-500";
+//    readonly white: "bg-white-500";
+// }
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
-  color?: string;
-  bg?: string;
+const btnColor = {
+  black: "text-black",
+  red: "text-red",
+  blue: "text-blue",
+  white: "text-white",
+} as const;
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  // bg?: 'gray' | 'blue' | 'red' | 'white' | 'gold';
+  // typeof btnBg: btnBg의 타입을 반환
+  // keyof typeof btnBg: btnBg의 타입의 키들을 유니온으로 반환
+  bg?: keyof typeof btnBg;
+  color?: keyof typeof btnColor;
 }
 
-function Button({ children, type='button', bg, color, ...rest }: ButtonProps){
+function Button({
+  children,
+  type = "button",
+  bg = "gray",
+  color = "black",
+  ...rest
+}: ButtonProps) {
   return (
-    <button type={ type } className={`${styles.button} ${styles[`bg-${bg}-text-${color}`]}`} { ...rest }>{ children }</button>
+    <button
+      type={type}
+      className={`${btnBg[bg]} border border-black/30 ${btnColor[color]} py-2 px-4 text-center no-underline inline-block text-base my-1 mx-0.5 cursor-pointer rounded-md`}
+      {...rest}
+    >
+      {children}
+    </button>
   );
 }
 
