5b322a7edd08fdc17026535afe270c3e75131f33
Lee HaYeong <dlgkdud4759@naver.com>
2025-12-24 15:39:17 +0900

    ch08 - 03 Button 컴포넌트에 styled-components 적용

--- ch08-css/03-styled/src/App.tsx
+++ ch08-css/03-styled/src/App.tsx
@@ -1,14 +1,17 @@
 import Button from "@components/ui/Button";
 
-function App(){
+function App() {
   return (
     <>
       <h1>03 Styled Components</h1>
 
       <Button>그냥 버튼</Button>
-      <Button type="reset" bg="blue" color="red">취소 버튼</Button>
-      <Button type="submit" bg="gray" color="blue">submit 버튼</Button>
-
+      <Button type="reset" $bg="red" $color="white">
+        취소 버튼
+      </Button>
+      <Button type="submit" $bg="blue" $color="white">
+        submit 버튼
+      </Button>
     </>
   );
 }
--- ch08-css/03-styled/src/components/ui/Button.tsx
+++ ch08-css/03-styled/src/components/ui/Button.tsx
@@ -1,13 +1,36 @@
-import styles from './Button.module.css';
+import styled from "styled-components";
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
-  color?: string;
-  bg?: string;
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  $color?: string;
+  $bg?: string;
 }
 
-function Button({ children, type='button', bg, color, ...rest }: ButtonProps){
+const BasicButtonStyle = styled.button<ButtonProps>`
+  background-color: ${(props) => props.$bg || "gray"};
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  color: ${(props) => props.$color || "black"};
+  padding: 6px 18px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 6px;
+`;
+
+function Button({
+  children,
+  type = "button",
+  $bg,
+  $color,
+  ...rest
+}: ButtonProps) {
+  const styledProps = { $bg, $color };
   return (
-    <button type={ type } className={`${styles.button} ${styles[`bg-${bg}-text-${color}`]}`} { ...rest }>{ children }</button>
+    <BasicButtonStyle type={type} {...rest} {...styledProps}>
+      {children}
+    </BasicButtonStyle>
   );
 }
 
