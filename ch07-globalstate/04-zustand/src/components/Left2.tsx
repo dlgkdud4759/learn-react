48c8a15da8435de1794f9d1019e0adc0b7eedbc1
Lee HaYeong <dlgkdud4759@naver.com>
2025-12-24 13:51:16 +0900

    ch07 - 04 zustand를 사용한 counter

--- ch07-globalstate/04-zustand/src/components/Left2.tsx
+++ ch07-globalstate/04-zustand/src/components/Left2.tsx
@@ -1,13 +1,21 @@
-import Left3 from '@components/Left3';
-import { useEffect } from 'react';
+import useCounterStore from "@/zustand/store";
+import Left3 from "@components/Left3";
+import { useEffect } from "react";
 
 function Left2() {
-  useEffect(()=>{
-    console.log('### Left2 렌더링.');
+  useEffect(() => {
+    console.log("### Left2 렌더링.");
   });
+
+  // store 전체 구독
+  // const { user } = useCounterStore();
+
+  // user만 선택적 구독
+  const user = useCounterStore((state) => state.user);
+
   return (
     <div>
-      <h2>Left2</h2>
+      <h2>Left2 - {user.name}</h2>
       <Left3 />
     </div>
   );
--- ch07-globalstate/04-zustand/src/components/Right3.tsx
+++ ch07-globalstate/04-zustand/src/components/Right3.tsx
@@ -1,13 +1,25 @@
-import { useEffect } from 'react';
+import useCounterStore from "@/zustand/store";
+import { useEffect } from "react";
 
 function Right3() {
-  useEffect(()=>{
-    console.log('#### Right3 렌더링.');
+  useEffect(() => {
+    console.log("#### Right3 렌더링.");
   });
+
+  // 상태 전체를 구독함
+  // const { countUp, countDown, countReset } = useCounterStore();
+
+  // 최적화를 위해서 필요한 상태만 선택적으로 구독
+  const countUp = useCounterStore((state) => state.countUp);
+  const countReset = useCounterStore((state) => state.countReset);
+  const countDown = useCounterStore((state) => state.countDown);
+
   return (
     <div>
       <h3>Right3</h3>
-      <button onClick={ () => {} }>+1</button>
+      <button onClick={() => countUp(1)}>+1</button>
+      <button onClick={() => countReset()}>0</button>
+      <button onClick={() => countDown(1)}>-1</button>
     </div>
   );
 }
