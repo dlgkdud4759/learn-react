f13bff4f5c7717cc6a6064de38406f9078002582
Lee HaYeong <dlgkdud4759@naver.com>
2025-12-30 15:16:51 +0900

    ch09 - 03 폴더 세팅

---
+++ ch09-ajax/03-board-reactquery/src/App.tsx
@@ -0,0 +1,12 @@
+import BoardInfo from "./pages/board/BoardInfo";
+
+function App() {
+  return (
+    <>
+      <h1>03 React Query(Tanstack Query) 라이브러리</h1>
+      <BoardInfo />
+    </>
+  );
+}
+
+export default App;
