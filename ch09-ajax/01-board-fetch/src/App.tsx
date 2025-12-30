efec39c5979a63af268f46e1ec6275d992e9a175
Lee HaYeong <dlgkdud4759@naver.com>
2025-12-30 13:36:16 +0900

    ch09 - 01 게시판 동작 구현 및 Bruno 사용

--- ch09-ajax/01-board-fetch/src/App.tsx
+++ ch09-ajax/01-board-fetch/src/App.tsx
@@ -1,7 +1,13 @@
+import BoardInfo from "./pages/board/BoardInfo";
+
 function App() {
   return (
-    <h1>vite-template</h1>
-  );  
+    // 2. public/boardinfo.html 파일과 동일한 화면이 보이도록 리액트 컴포넌트 만들기
+    <>
+      <h1>01 Fetch API</h1>
+      <BoardInfo />
+    </>
+  );
 }
 
-export default App
+export default App;
--- ch09-ajax/01-board-fetch/src/types/board.ts
+++ ch09-ajax/01-board-fetch/src/types/board.ts
@@ -1,2 +1,66 @@
 // 코드 참고: https://github.com/FEBC-15/react/blob/main/workspace-ins/ch12-app/todolist/06-api/src/types/todo.ts
 // API 참고: https://fesp-api.koyeb.app/market/apidocs/#/게시판
+
+// TODO 2: 타입 추가
+// 아이템 타입
+export interface BoardInfo {
+  _id: number;
+  title: string;
+  content: string;
+}
+
+// 목록 조회 결과 타입
+export interface BoardListRes {
+  ok: 1;
+  item: BoardInfo[];
+}
+
+// 상세 조회 결과 타입
+export interface BoardInfoRes {
+  ok: 1;
+  item: BoardInfo;
+}
+
+// 댓글 타입
+export interface BoardReply {
+  _id: number;
+  content: string;
+}
+
+// 댓글 목록 조회 결과 타입
+export interface BoardReplyListRes {
+  ok: 1;
+  item: BoardReply[];
+}
+
+// 댓글 등록 결과 타입
+export interface BoardReplyCreateRes {
+  ok: 1;
+  item: BoardReply;
+}
+
+// 데이터 검증 오류 타입
+interface FieldError {
+  type: string;
+  value: string;
+  msg: string;
+  location: string;
+}
+
+// 서버에서 에러를 응답할 경우
+export interface ErrorRes {
+  ok: 0;
+  message: string;
+  errors?: {
+    [field: string]: FieldError;
+  };
+}
+
+// 서버의 응답
+export type ResData<
+  T extends
+    | BoardListRes
+    | BoardInfoRes
+    | BoardReplyListRes
+    | BoardReplyCreateRes
+> = T | ErrorRes;
