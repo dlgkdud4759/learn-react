249d2755b9c6b589592d6998ac4acbfca20ad889
Lee HaYeong <dlgkdud4759@naver.com>
2025-12-31 15:31:20 +0900

    ch09 - 04 Fetch-On-Render, Fetch-Then-Render 방식 적용

--- ch09-ajax/04-board-pattern/src/FetchOnRender.tsx
+++ ch09-ajax/04-board-pattern/src/FetchOnRender.tsx
@@ -1,20 +1,56 @@
+import axios from "axios";
+import type {
+  BoardInfo,
+  BoardInfoRes,
+  BoardReply,
+  BoardReplyListRes,
+} from "./types/board";
+import { useEffect, useState } from "react";
+
 // 게시글 상세 조회 API 호출
 function fetchPost() {
-  
+  return axios
+    .get<BoardInfoRes>("https://fesp-api.koyeb.app/market/posts/10", {
+      headers: {
+        "Client-Id": "openmarket",
+      },
+    })
+    .then((response) => response.data.item);
 }
 
 // 댓글 목록 조회 API 호출
 function fetchComments() {
-  
+  return axios
+    .get<BoardReplyListRes>(
+      "https://fesp-api.koyeb.app/market/posts/10/replies",
+      {
+        headers: {
+          "Client-Id": "openmarket",
+        },
+      }
+    )
+    .then((response) => response.data.item);
 }
 
 // 게시글 상세 조회 화면
 function FetchOnRender() {
   // TODO: 게시글 상세 조회
+  const [data, setData] = useState<BoardInfo>();
+
+  useEffect(() => {
+    fetchPost().then(setData);
+  }, []);
+
+  // 데이터가 준비되기 전에는 대체 컨텐츠를 반환
+  if (!data) {
+    return <h4>게시물 제목 로딩중...</h4>;
+  }
 
   return (
     <>
-      <h4>1번 게시물 제목</h4>
+      <h4>
+        {data._id} - {data.title}
+      </h4>
       <Comments />
     </>
   );
@@ -23,14 +59,22 @@ function FetchOnRender() {
 // 댓글 목록을 조회해서 출력하는 컴포넌트
 export function Comments() {
   // TODO: 댓글 목록 조회
-  
+  const [data, setData] = useState<BoardReply[]>();
+
+  useEffect(() => {
+    fetchComments().then(setData);
+  }, []);
+
+  // 데이터가 준비되기 전에는 대체 컨텐츠를 반환
+  if (!data) {
+    return <h4>게시물 댓글 로딩중...</h4>;
+  }
+
+  const list = data.map((reply) => <li key={reply._id}>{reply.content}</li>);
+
   return (
     <>
-      <ul>
-        <li>댓글 1</li>
-        <li>댓글 2</li>
-        <li>댓글 3</li>
-      </ul>
+      <ul>{list}</ul>
     </>
   );
 }
--- ch09-ajax/04-board-pattern/src/PostList.tsx
+++ ch09-ajax/04-board-pattern/src/PostList.tsx
@@ -1,17 +1,38 @@
 import FetchOnRender from "@/FetchOnRender";
-import FetchThenRender from "@/FetchThenRender";
+import axios from "axios";
+import type { BoardList, BoardListRes } from "./types/board";
+import { useEffect, useState } from "react";
+import FetchThenRender from "./FetchThenRender";
 
 // 게시물 목록 조회 API 호출
-function fetchList(){
-  
+function fetchList() {
+  return axios
+    .get<BoardListRes>("https://fesp-api.koyeb.app/market/posts", {
+      headers: {
+        "Client-Id": "openmarket",
+      },
+    })
+    .then((response) => response.data.item);
 }
 
-function PostList(){
+// 참고: https://github.com/FEBC-15/js/blob/main/docs/08.js_etc.md#4-동기-방식과-비동기-방식
+function PostList() {
   // TODO: 게시물 목록 조회
+  const [data, setData] = useState<BoardList[]>();
 
+  useEffect(() => {
+    fetchList().then(setData);
+  }, []);
+
+  // 데이터가 준비되기 전에는 대체 컨텐츠를 반환
+  if (!data) {
+    return <h2>게시물 목록 로딩중...</h2>;
+  }
+
+  // 데이터가 준비된 이후에 렌더링 됨
   return (
     <>
-      <h2>게시물 5건.</h2>
+      <h2>게시물 {data.length}건.</h2>
       <hr />
       <h3>Fetch-On-Render 방식(폭포수 현상)</h3>
       <FetchOnRender />
