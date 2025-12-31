32c11c1258f79384e61c03372b71bc03342caea3
Lee HaYeong <dlgkdud4759@naver.com>
2025-12-31 10:16:51 +0900

    ch09 - 03 댓글 목록 컴포넌트 속성(staleTime, refetchOnWindowFocus, refetchInterval) 추가

--- ch09-ajax/03-board-reactquery/src/pages/board/CommentList.tsx
+++ ch09-ajax/03-board-reactquery/src/pages/board/CommentList.tsx
@@ -11,6 +11,9 @@ function CommentList() {
     queryKey: ["posts", "3", "replies"],
     queryFn: () => axiosInstance.get<BoardReplyListRes>("/posts/3/replies"),
     select: (response) => response.data.item,
+    staleTime: 1000 * 10, // 일정 시간동안 캐시해서 서버 호출 횟수를 줄임
+    refetchOnWindowFocus: false, // 다른 탭이나 앱에서 작업 후에 돌아오면 데이터를 자동으로 갱신
+    refetchInterval: 1000 * 2, // 주기적으로 호출해서 데이터 갱신을 자동화
   });
 
   const list = data?.map((reply) => <li key={reply._id}>{reply.content}</li>);
