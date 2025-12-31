ed847f6c3609e9768c524dddd36c3988d43157a4
Lee HaYeong <dlgkdud4759@naver.com>
2025-12-31 16:04:47 +0900

    ch09 - 05 Render-as-you-fetch(Suspense)

--- ch09-ajax/05-board-suspense/src/FetchAsYouRender.tsx
+++ ch09-ajax/05-board-suspense/src/FetchAsYouRender.tsx
@@ -1,36 +1,38 @@
-import type { BoardInfo, BoardInfoRes, BoardReply, BoardReplyListRes } from "@/types/board";
+import type { BoardInfoRes, BoardReplyListRes } from "@/types/board";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { use } from "react";
 
 // 게시글 상세 조회 API 호출
 function fetchPost() {
-  return axios.get<BoardInfoRes>('https://fesp-api.koyeb.app/market/posts/1?delay=3000', {
-    headers: {
-      'client-id': 'openmarket'
-    }
-  }).then(res => res.data.item);
+  return axios
+    .get<BoardInfoRes>("https://fesp-api.koyeb.app/market/posts/1?delay=3000", {
+      headers: {
+        "client-id": "openmarket",
+      },
+    })
+    .then((res) => res.data.item);
 }
 
 // 댓글 목록 조회 API 호출
 function fetchComments() {
-  return axios.get<BoardReplyListRes>('https://fesp-api.koyeb.app/market/posts/1/replies?page=1&limit=3&delay=2000', {
-    headers: {
-      'client-id': 'openmarket'
-    }
-  }).then(res => res.data.item);
+  return axios
+    .get<BoardReplyListRes>(
+      "https://fesp-api.koyeb.app/market/posts/1/replies?page=1&limit=3&delay=2000",
+      {
+        headers: {
+          "client-id": "openmarket",
+        },
+      }
+    )
+    .then((res) => res.data.item);
 }
+
+const postPromise = fetchPost();
+const commentsPromise = fetchComments();
 
 // 게시글 상세 조회 화면
 function FetchAsYouRender() {
-  const [data, setData] = useState<BoardInfo>();
-
-  useEffect(() => {
-    fetchPost().then(setData);
-  }, []);
-
-  if(!data){
-    return <div>1번 게시물 로딩중...</div>;
-  }
+  const data = use(postPromise);
 
   return (
     <>
@@ -42,21 +44,13 @@ function FetchAsYouRender() {
 
 // 댓글 목록 조회 화면
 export function Comments() {
-  const [data, setData] = useState<BoardReply[]>();
-
-  useEffect(() => {
-    fetchComments().then(setData);
-  }, []);
-
-  if(!data){
-    return <div>댓글 로딩중...</div>;
-  }
+  const data = use(commentsPromise);
 
   const list = data.map((item) => <li key={item._id}>{item.content}</li>);
 
   return (
     <>
-      <ul>{ list }</ul>
+      <ul>{list}</ul>
     </>
   );
 }
