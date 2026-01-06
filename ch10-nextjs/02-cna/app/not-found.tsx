32ac09cd8f1b4fe2a230dcb81852b331a571e287
Lee HaYeong <dlgkdud4759@naver.com>
2026-01-06 14:12:14 +0900

    ch10 - 02 NotFound 컴포넌트 적용

---
+++ ch10-nextjs/02-cna/app/not-found.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>404 - 페이지를 찾을 수 없습니다.</h1>
+      <Link href="/">홈으로 이동</Link>
+    </div>
+  );
+}
