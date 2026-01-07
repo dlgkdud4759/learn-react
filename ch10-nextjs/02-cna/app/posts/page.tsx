b86328f1facac6d710fe5082f729045328582082
Lee HaYeong <dlgkdud4759@naver.com>
2026-01-07 09:47:29 +0900

    ch10 - 02 PostPage type 추가

--- ch10-nextjs/02-cna/app/posts/page.tsx
+++ ch10-nextjs/02-cna/app/posts/page.tsx
@@ -1,3 +1,4 @@
+import { ErrorRes, PostListRes } from "@/types";
 import { Metadata } from "next";
 import Link from "next/link";
 
@@ -12,7 +13,11 @@ export default async function PostsPage() {
 
   // 서버 컴포넌트에서는 route handler를 호출할 필요는 없음
   const res = await fetch("http://localhost:3000/api/posts");
-  const data = await res.json();
+  const data: PostListRes | ErrorRes = await res.json();
+
+  if (data.ok === 0) {
+    return "로딩중";
+  }
 
   const list = data.item.map((post) => (
     <li key={post._id}>
