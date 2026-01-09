5bd1783d6bdd1e420dbcbf0f114285ab0209b876
dlgkdud4759 <dlgkdud4759@naver.com>
2026-01-09 14:26:35 +0900

    ch11 - lion-board-next 03 게시물 등록 화면(게시물 등록 및 에러 메시지 변경, 중복 클릭 방지)

--- ch11-skeleton/lion-board-next-03/app/[boardType]/new/page.tsx
+++ ch11-skeleton/lion-board-next-03/app/[boardType]/new/page.tsx
@@ -1,5 +1,5 @@
+import RegistForm from "@/app/[boardType]/new/RegistForm";
 import { Metadata } from "next";
-import Link from "next/link";
 
 export async function generateMetadata({
   params,
@@ -36,53 +36,7 @@ export default async function NewPage({
         </h2>
       </div>
       <section className="mb-8 p-4">
-        <form action={`/${boardType}`}>
-          <div className="my-4">
-            <label className="block text-lg content-center" htmlFor="title">
-              제목
-            </label>
-            <input
-              id="title"
-              type="text"
-              placeholder="제목을 입력하세요."
-              className="w-full py-2 px-4 border rounded-md dark:bg-gray-700 border-gray-300 focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200"
-              name="title"
-            />
-            <p className="ml-2 mt-1 text-sm text-red-500 dark:text-red-400">
-              제목은 필수입니다.
-            </p>
-          </div>
-          <div className="my-4">
-            <label className="block text-lg content-center" htmlFor="content">
-              내용
-            </label>
-            <textarea
-              id="content"
-              rows={15}
-              placeholder="내용을 입력하세요."
-              className="w-full p-4 text-sm border rounded-lg border-gray-300 bg-gray-50 focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-              name="content"
-            ></textarea>
-            <p className="ml-2 mt-1 text-sm text-red-500 dark:text-red-400">
-              내용은 필수입니다.
-            </p>
-          </div>
-          <hr />
-          <div className="flex justify-end my-6">
-            <button
-              type="submit"
-              className="bg-orange-500 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded"
-            >
-              등록
-            </button>
-            <Link
-              href={`/${boardType}`}
-              className="bg-gray-900 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded"
-            >
-              취소
-            </Link>
-          </div>
-        </form>
+        <RegistForm boardType={boardType} />
       </section>
     </main>
   );
