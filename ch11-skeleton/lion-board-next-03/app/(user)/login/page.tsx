f6471d25212d7cb50c825bc65fd4d1c35d0d772e
dlgkdud4759 <dlgkdud4759@naver.com>
2026-01-09 15:42:32 +0900

    ch11 - lion-board-next 03 로그인 화면(클라이언트 컴포넌트 분리 및 서버 액션 연동, 로그인 기능 구현)

--- ch11-skeleton/lion-board-next-03/app/(user)/login/page.tsx
+++ ch11-skeleton/lion-board-next-03/app/(user)/login/page.tsx
@@ -1,5 +1,5 @@
+import LoginForm from "@/app/(user)/login/LoginForm";
 import { Metadata } from "next";
-import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "로그인 - 라이언 보드",
@@ -20,63 +20,7 @@ export default async function LoginPage() {
           </h2>
         </div>
 
-        <form action="/">
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 dark:text-gray-200 mb-2"
-              htmlFor="email"
-            >
-              이메일
-            </label>
-            <input
-              id="email"
-              type="email"
-              autoComplete="email"
-              placeholder="이메일을 입력하세요"
-              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-orange-400 dark:bg-gray-700"
-              name="email"
-            />
-            <p className="ml-2 mt-1 text-sm text-red-500 dark:text-red-400">
-              이메일은 필수입니다.
-            </p>
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 dark:text-gray-200 mb-2"
-              htmlFor="password"
-            >
-              비밀번호
-            </label>
-            <input
-              id="password"
-              type="password"
-              autoComplete="current-password"
-              placeholder="비밀번호를 입력하세요"
-              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-orange-400 dark:bg-gray-700"
-              name="password"
-            />
-            <p className="ml-2 mt-1 text-sm text-red-500 dark:text-red-400">
-              비밀번호는 필수입니다.
-            </p>
-            <Link
-              href="#"
-              className="block mt-6 ml-auto text-gray-500 text-sm dark:text-gray-300 hover:underline"
-            >
-              비밀번호를 잊으셨나요?
-            </Link>
-          </div>
-          <div className="mt-10 flex justify-center items-center">
-            <button
-              type="submit"
-              className="bg-orange-500 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded"
-            >
-              로그인
-            </button>
-            <Link href="/signup" className="ml-8 text-gray-800 hover:underline">
-              회원가입
-            </Link>
-          </div>
-        </form>
+        <LoginForm />
       </div>
     </main>
   );
